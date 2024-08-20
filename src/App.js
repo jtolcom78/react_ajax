4f46b25b1028c40ac0d33ae42e808cf593dd8197
SeokHyun Jee <jtolcom78@gmail.com>
2024-08-20 10:58:57 +0700

    s2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+
+class Nav extends Component {
+  componentDidMount() {
+    fetch('list.json')
+      .then(function(result){
+        return result.json();
+      })
+        .then(function(json){
+          console.log(json);
+      })
+  }
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1">HTML</a></li>
+          <li><a href="2">CSS</a></li>
+          <li><a href="3">JS</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WEB</h1>
+      <Nav></Nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; Ajax
+      </article>
     </div>
   );
 }
