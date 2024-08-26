d848ee22e560f561bda7907aedce81238939d8fb
SeokHyun Jee <jtolcom78@gmail.com>
2024-08-26 16:02:40 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,115 @@
 import { Component } from "react";
 
 class Nav extends Component {
-  componentDidMount() {
-    fetch('list.json')
-      .then(function(result){
-        return result.json();
-      })
-        .then(function(json){
-          console.log(json);
-      })
-  }
   render() {
+    var listTag = [];
+    for (var i = 0; i < this.props.list.length; i++) {
+      var li = this.props.list[i];
+      listTag.push(
+        <li key={li.id}>
+          <a href={li.id} data-id={li.id} onClick={function (e) {
+            e.preventDefault();
+            this.props.onClick(e.target.dataset.id)
+          }.bind(this)}>
+            {li.title}
+          </a>
+        </li>
+      )
+    }
     return (
       <nav>
         <ul>
-          <li><a href="1">HTML</a></li>
-          <li><a href="2">CSS</a></li>
-          <li><a href="3">JS</a></li>
+          {listTag}
         </ul>
       </nav>
     );
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
-      <Nav></Nav>
+class Article extends Component {
+  render() {
+    return (
       <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
-    </div>
-  );
+    )
+  }
+}
+
+class NowLoading extends Component {
+  render() {
+    return <div>Now Loading...</div>
+  }
+}
+
+class App extends Component {
+  state = {
+    article: {
+      item: { title: 'Welcome', desc: 'Hello, React & Ajax' },
+      isLoading: false
+    },
+    list:
+    {
+      items: [],
+      isLoading: false
+    }
+  }
+  componentDidMount() {
+    var Newlist = Object.assign({}, this.state.list, { isLoading: true });
+    this.setState({ list: Newlist });
+
+    fetch('list.json')
+      .then(function (result) {
+        return result.json();
+      })
+      .then(function (json) {
+        this.setState({
+          list: {
+            items: json,
+            isLoading: false
+          }
+        });
+      }.bind(this));
+  }
+  render() {
+    var NavTag = null;
+    if (this.state.list.isLoading) {
+      NavTag = <NowLoading></NowLoading>
+    } else {
+      NavTag = <Nav list={this.state.list.items} onClick={function (id) {
+        var newArticle = Object.assign({}, this.state.article, { isLoading: true });
+        this.setState({ article: newArticle });
+        fetch(id + '.json')
+          .then(function (result) {
+            return result.json();
+          })
+          .then(function (json) {
+            this.setState({
+              article: {
+                item: { title: json.title, desc: json.desc },
+                isLoading: false
+              }
+            })
+          }.bind(this));
+      }.bind(this)}></Nav>
+    }
+
+    var ArticleTag = null;
+    if (this.state.article.isLoading) {
+      ArticleTag = <NowLoading></NowLoading>
+    } else {
+      ArticleTag = <Article title={this.state.article.item.title} desc={this.state.article.item.desc}></Article>
+    }
+
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        {NavTag}
+        {ArticleTag}
+      </div>
+    )
+  }
 }
 
 export default App;
